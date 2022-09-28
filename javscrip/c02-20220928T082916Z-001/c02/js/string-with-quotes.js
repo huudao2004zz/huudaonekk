35a698b2d5b67ea819ff96acebe71d5dc197b3f0
Your Name <nguyenhuudao3211@gmail.com>
2022-09-28 16:22:56 +0700

    jassss

---
+++ javscrip/c02-20220928T082916Z-001/c02/js/string-with-quotes.js
@@ -0,0 +1,8 @@
+var title;
+var message;
+tile = "Molly's Special Offers";
+message = '<a href = \"sale.html\" >25% off ! </a>';
+var eltitle = document.getElementById('title');
+eltitle.textContent = title;
+var elnote = document.getElementById('note');
+elnote.innerHTML = message ;
